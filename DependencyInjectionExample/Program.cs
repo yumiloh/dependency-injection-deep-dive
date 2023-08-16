620f48304604ad94ced92e2dbed93b26480ec72b
Yumi (Mun Suen) Loh <yumilohxct@gmail.com>
2023-08-16 21:41:01 +0800

    Update Program.cs

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -6,10 +6,7 @@ namespace DependencyInjectionExample
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -19,12 +16,10 @@ namespace DependencyInjectionExample
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseHttpsRedirection();
 
